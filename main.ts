9ed5189ae934366e82ca35a6989e1e2fb5234696
Michael Braun <57547002+michaelbraunrentonschools@users.noreply.github.com>
2019-12-17 13:33:31 -0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,54 @@
+namespace SpriteKind {
+    export const object = SpriteKind.create()
+}
+sprites.onOverlap(SpriteKind.Player, SpriteKind.object, function (sprite, otherSprite) {
+    info.changeScoreBy(1)
+    jared.vy += -250
+    trampo.setKind(SpriteKind.object)
+    trampo.x += Math.randomRange(-70, 70)
+    trampo.y += Math.randomRange(-50, -60)
+})
+let trampo: Sprite = null
+let jared: Sprite = null
+scene.setBackgroundColor(15)
+jared = sprites.create(img`
+f f f f f 5 5 5 5 5 f f f f f f 
+f f 5 5 5 5 f f f f 5 5 5 5 f f 
+f f 5 f f f f f f f f f f 5 5 5 
+f 5 f f 9 f f f f f 9 f f f f 5 
+5 5 f 9 9 f f f f f 9 9 f f f 5 
+5 f f f f f f 9 9 f f f f f f 5 
+5 f f f f f f 9 9 f f f f f f 5 
+5 5 f f f f f f f f f f f f 5 5 
+f 5 f f f 5 5 5 5 5 5 5 5 f 5 f 
+5 f 5 5 5 5 f f f f f 5 5 5 f 5 
+5 f f 5 f f f f f f f f f 5 f 5 
+5 5 5 5 5 5 f f f f 5 5 5 5 5 5 
+f f f 5 5 5 f f f f 5 5 5 f f f 
+f f f 5 5 5 f f f f 5 5 5 f f f 
+f f f 5 5 5 f f f f 5 5 5 f f f 
+f f f 5 5 5 f f f f 5 5 5 f f f 
+`, SpriteKind.Player)
+scene.cameraFollowSprite(jared)
+jared.ay = 100
+trampo = sprites.create(img`
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
+1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
+5 f f f f f f 5 f f f f f f f 5 
+f 5 5 f f 5 5 f 5 5 f f f 5 5 f 
+f f 5 f 5 f f f f f 5 f 5 f f f 
+f f f 5 f f f f f f f 5 f f f f 
+1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
+1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+`, SpriteKind.object)
+game.onUpdate(function () {
+    jared.x += controller.dx()
+})
